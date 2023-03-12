18e7ebbf0b3d9f2b4700458366b6c3fa39a57c49
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-12 10:49:24 -0400

    Initial attempt at making a story using tutorial

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,21 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+import App from "./App";
+
+export default {
+  title: "App",
+  component: App,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => <App {...args} />;
+
+// 👇 Each story then reuses that template
+export const Primary = Template.bind({});
+Primary.args = { backgroundColor: '#ff0', label: 'App' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, label: '😄👍😍💯' };
+
+export const Tertiary = Template.bind({});
+Tertiary.args = { ...Primary.args, label: '📚📕📈🤓' };
