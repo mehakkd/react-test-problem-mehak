d2fc581460e553db88c4383ba3b65d1836eff782
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-12 11:06:57 -0400

    Added button and handler (first try) App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,12 +10,26 @@ export default class App extends React.Component {
   }
 
   //handler goes here
+  //https://reactgo.com/react-change-button-text-onclick/
+  function changeText() {
+    const [active, setActive] = useState(false);
+    const handleClick = () => {
+      setActive(!active);
+    };
+  }
 
+  
+  //what does render() meaN?
   render() {
     return (
       <div>
         <p>Greetings!</p>
         <p>Button goes here</p>
+      
+        <button type= "button" onClick= {handleClick}>
+          {active ? "click me" : "thanks"}
+        </button>
+
       </div>
     );
   }
