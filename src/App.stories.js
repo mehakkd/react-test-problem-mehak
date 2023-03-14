3744dec8a5feda5509e2570c36f1f1a96d2b32f9
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 12:44:13 -0400

    update to import App from ./App.js instead of ./App

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,6 +1,6 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
-import App from "./App";
+import App from "./App.js";
 
 export default {
   title: "App",
