d880ac84f4760b7b66ed666caf9fa380e5d9e0c7
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 13:02:51 -0400

    just a test

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,16 @@ import { App } from './App.stories'; //👈 Our stories imported here.
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
 
+const Goodbye = () => {
+  return <h1>Bye Everyone</h1>
+};
+
+test('should print the Goodbye component', () => {
+  render(<Goodbye/>);
+  screen.debug();
+});
+
+/*
 it('Check that the button initially displays click me', () => {
   //const { getByTestId, getByText } = render(<App {...App.args} />);
    
@@ -20,3 +30,4 @@ it('Checks that the button changes text to thanks after click', () => {
 
   expect(screen.getByText("thanks")).toBeInTheDocument()
 });
+*/
