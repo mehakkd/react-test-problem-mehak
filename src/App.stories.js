45a46929fc259cbe2f32abf8c558a6a1329a1929
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 14:43:57 -0400

    back to version 1

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,27 +1,19 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
 
-import React from 'react';
-
-import { ComponentMeta } from '@storybook/react';
-
-import { App } from './App';
+import App from "./App";
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'App',
+  title: "App",
   component: App,
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
+const Template = (args) => <App {...args} />;
 
-//👇 Each story then reuses that template
+// 👇 Each story then reuses that template
 export const Primary = Template.bind({});
-Primary.args = { backgroundColor: '#ff0', label: 'Primary' };
+Primary.args = { backgroundColor: '#ff0', label: 'App' };
 
 export const Secondary = Template.bind({});
 Secondary.args = { ...Primary.args, label: '😄👍😍💯' };
