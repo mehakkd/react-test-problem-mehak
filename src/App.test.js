9da5d2b809d576dad4e8c5a3b69d750167597c5f
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 12:31:10 -0400

    initial attempt writing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import { App } from './App.stories'; //👈 Our stories imported here.
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+
+it('Check that the button initially displays click me', () => {
+  const { getByTestId, getByText } = render(<App {...App.args} />);
+   
+  const clickme = getByTestId(`click-me`)
+  expect(clickme.innerHTML).toBe("click me")
+});
+
+it('Checks that the button changes text to thanks after click', () => {
+  const { getByTestId, getByText } = render(<InvalidForm {...InvalidForm.args} />);
+
+  fireEvent.click(getByText('click me'));
+
+  const thanks = getByTestId(`thanks`)
+  expect(thanks.innerHTML).toBe("thanks")
+});
