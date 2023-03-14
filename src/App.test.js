52e3944d685b57230ec760f99e65683f708db76a
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 12:53:18 -0400

    checking through "screen" and "getByLabelText"

--- src/App.test.js
+++ src/App.test.js
@@ -7,17 +7,17 @@ import { App } from './App.stories'; //👈 Our stories imported here.
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
 
 it('Check that the button initially displays click me', () => {
-  const { getByTestId, getByText } = render(<App {...App.args} />);
+  //const { getByTestId, getByText } = render(<App {...App.args} />);
    
-  const clickme = getByTestId(`click-me`)
+  const clickme = screen.getByLabelText("click me")
   expect(clickme.innerHTML).toBe("click me")
 });
 
 it('Checks that the button changes text to thanks after click', () => {
-  const { getByTestId, getByText } = render(<App {...App.args} />);
+  //const { getByTestId, getByText } = render(<App {...App.args} />);
 
-  fireEvent.click(getByText('click me'));
+  fireEvent.click(screen.getByLabelText("click me"));
 
-  const thanks = getByTestId(`thanks`)
+  const thanks = screen.getByLabelText("thanks")
   expect(thanks.innerHTML).toBe("thanks")
 });
