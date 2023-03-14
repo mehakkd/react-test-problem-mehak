41c265f27d71c3e41a59b19c69d04e77698622f5
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 12:48:24 -0400

    imported react

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,5 +1,6 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+import React from 'react';
 import App from "./App.js";
 
 export default {
