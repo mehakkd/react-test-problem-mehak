3fc86ee290cf68162c9eb6d0a91269ff6d5b474e
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 21:41:37 -0400

    removed old commented code

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {active: true};
-    
-    // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
   }
 
   //handler goes here
-  //https://reactjs.org/docs/handling-events.html
   handleClick() {
     this.setState(prevState => ({
       active: !prevState.active
