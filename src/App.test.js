68f3a1d62fab9ae1f924b52a41c4b470068409f6
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 21:34:28 -0400

    imported Primary directly from App.stories.js instead of using composeStories

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,17 @@
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
 import { render, screen, fireEvent } from '@testing-library/react';
+/*
 import { composeStories } from '@storybook/testing-react';
-
 import * as stories from './App.stories'; // import all stories from the stories file
 
 // Every component that is returned maps 1:1 with the stories, but they already contain all decorators from story level, meta level and global level.
-const { Primary } = composeStories(stories);
 //composeStories will process all stories from the component you specify, 
 //compose args/decorators in all of them and return an object containing the composed stories
+const { Primary } = composeStories(stories);
+*/
 
+import { Primary } from './App.stories';
 
 it('Check that the button initially displays click me', () => {
   render (<Primary />)
@@ -17,7 +19,7 @@ it('Check that the button initially displays click me', () => {
   expect(clickme).toBeInTheDocument()
 });
 
-it('Checks that the button changes text to thanks after click', () => {
+it('Checks that the button changes text to 'thanks' after click', () => {
   render (<Primary />)
   fireEvent.click(screen.getByText("click me"));
   const thanks = screen.getByText("thanks")
