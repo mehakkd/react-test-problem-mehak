91fae9755a0978bc49df2b2185bd0ce12977ea9d
mehakkd <70606001+mehakkd@users.noreply.github.com>
2023-03-14 21:20:36 -0400

    used composeStories

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,24 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-
-// import your story from App.stories.js
-import { App } from './App.stories'; //👈 Our stories imported here.
-
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+import * as stories from './App.stories'; // import all stories from the stories file
+
+// Every component that is returned maps 1:1 with the stories, but they already contain all decorators from story level, meta level and global level.
+const { Primary } = composeStories(stories);
+//composeStories will process all stories from the component you specify, 
+//compose args/decorators in all of them and return an object containing the composed stories
+
 
 it('Check that the button initially displays click me', () => {
-  //const { getByTestId, getByText } = render(<App {...App.args} />);
-   
+  render (<Primary />)
   const clickme = screen.getByText("click me")
   expect(clickme).toBeInTheDocument()
 });
 
 it('Checks that the button changes text to thanks after click', () => {
-  //const { getByTestId, getByText } = render(<App {...App.args} />);
-
+  render (<Primary />)
   fireEvent.click(screen.getByText("click me"));
-
-  expect(screen.getByText("thanks")).toBeInTheDocument()
+  const thanks = screen.getByText("thanks")
+  expect(thanks).toBeInTheDocument()
 });
 
